3bd78401d7b19844c230b49c5c86a313fb4a86c2
Sebastian <sebassdc@gmail.com>
2016-05-06 04:45:42 -0400

    Directory organized

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express'),
+client_track = require('client_track');
 app = express();
 
-
-// Mi codigo
-
 app.set('port', (process.env.PORT || 8080));
+
 app.use(express.static(__dirname + '/public'));
+
+// Setting de views
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+  client_track.reqlog(req);
+  res.render('index')
+});
+
 app.listen(app.get('port'), function(){
   console.log('Node corriendo en el puerto', app.get('port'));
 });
